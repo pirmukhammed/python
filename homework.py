f1ae933d97fe0d0ef73c0c03ad9afd7402998f3d
pirmukhammed <75626464+pirmukhammed@users.noreply.github.com>
2020-12-23 15:34:57 +0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,11 +1,15 @@
+'''homework8'''
 import csv
 
 myData = [["id","username","email","ip_address"],
 			['1','admin','admin@example.com','192.168.0.1'], 
 			['2','test_user','test_user@example.com','192.168.0.2'],
 			['3','second_user','second_user@example.com','192.168.0.3']]
-
+'''Вариант с местом указания файла'''
 myFile = open('C:\\Users\\Home\\Desktop\\python\\Урок8\\data.csv', 'w')
+
+'''Вариант файла с кодами'''
+myFile = open('data.csv', 'w')
 with myFile:
 		writer = csv.writer(myFile)
 		writer.writerows(myData)
