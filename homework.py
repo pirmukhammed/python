a758c596b3cc8cdbf275f6f182652e32b3056153
pirmukhammed <75626464+pirmukhammed@users.noreply.github.com>
2020-12-24 15:41:15 +0600

    Update homework.py

--- homework.py
+++ homework.py
@@ -6,14 +6,14 @@ myData = [["id","username","email","ip_address"],
 			['2','test_user','test_user@example.com','192.168.0.2'],
 			['3','second_user','second_user@example.com','192.168.0.3']]
 '''Вариант с местом указания файла'''
-myFile = open('C:\\Users\\Home\\Desktop\\python\\Урок8\\data.csv', 'w')
+myFile = open('C:\\Users\\Home\\Desktop\\python\\Урок8\\homewok8_data.csv', 'w')
 
 '''Вариант файла с кодами'''
-myFile = open('data.csv', 'w')
+'''myFile = open('data.csv', 'w')'''
 with myFile:
 		writer = csv.writer(myFile)
 		writer.writerows(myData)
 
-myFile = open('C:\\Users\\Home\\Desktop\\python\\Урок8\\data.csv', 'r')
+myFile = open('C:\\Users\\Home\\Desktop\\python\\Урок8\\homewok8_data.csv', 'r')
 print(myFile.read())
 print('Записи записаны') 
